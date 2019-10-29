7ff5993d38c72a4ab2eff458ce9cdba775ae34cf
dromanic <Denis Romanichenko dentair4004@gmail.com>
2019-10-30 00:42:00 +0200

    fix sens mouse

--- sources/keys_mouse.c
+++ sources/keys_mouse.c
@@ -6,7 +6,7 @@
 /*   By: dromanic <dromanic@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 15:36:02 by dromanic          #+#    #+#             */
-/*   Updated: 2019/10/23 18:16:11 by dromanic         ###   ########.fr       */
+/*   Updated: 2019/10/30 00:37:30 by dromanic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ _Bool		mouse_move(t_env *env, SDL_Event *event)
 	if (event->button.button == SDL_BUTTON_LEFT)
 	{
 		if (event->motion.xrel < -2)
-			env->flags.move.x = NEG;
+			env->flags.move.x = NEG * 2;
 		else if (event->motion.xrel > 2)
-			env->flags.move.x = POS;
+			env->flags.move.x = POS * 2;
 		if (event->motion.yrel < -2)
-			env->flags.move.y = POS;
+			env->flags.move.y = POS * 2;
 		else if (event->motion.yrel > 2)
-			env->flags.move.y = NEG;
+			env->flags.move.y = NEG * 2;
 		if (event->motion.xrel || event->motion.yrel)
 			return (true);
 	}
@@ -35,13 +35,13 @@ _Bool		mouse_rotate(t_env *env, SDL_Event *event)
 	if (event->button.button == SDL_BUTTON_X1)
 	{
 		if (event->motion.xrel < -2)
-			env->flags.rotate.x = POS;
+			env->flags.rotate.x = NEG * 2;
 		else if (event->motion.xrel > 2)
-			env->flags.rotate.x = NEG;
+			env->flags.rotate.x = POS * 2;
 		if (event->motion.yrel < -2)
-			env->flags.rotate.y = NEG;
+			env->flags.rotate.y = POS * 2;
 		else if (event->motion.yrel > 2)
-			env->flags.rotate.y = POS;
+			env->flags.rotate.y = NEG * 2;
 		if (event->motion.xrel || event->motion.yrel)
 			return (true);
 	}
@@ -53,9 +53,9 @@ _Bool		mouse_rotate_z(t_env *env, SDL_Event *event)
 	if (event->button.button == SDL_BUTTON_MIDDLE)
 	{
 		if (event->motion.xrel < 0)
-			env->flags.rotate.z = NEG;
+			env->flags.rotate.z = NEG * 2;
 		else if (event->motion.xrel > 0)
-			env->flags.rotate.z = POS;
+			env->flags.rotate.z = POS * 2;
 		if (event->motion.xrel)
 			return (true);
 	}
