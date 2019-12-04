e514a43bf828c1a19b69cf05e579accf73f3a934
dromanic <Denis Romanichenko dentair4004@gmail.com>
2019-12-04 12:07:50 +0200

    cross platforming auto-switchers added

--- libraries/libft/ft_get_processors_num.c
+++ libraries/libft/ft_get_processors_num.c
@@ -3,41 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   ft_get_processors_num.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dromanic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dromanic <dromanic@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/12 12:51:00 by dromanic          #+#    #+#             */
-/*   Updated: 2018/11/03 14:45:47 by dromanic         ###   ########.fr       */
+/*   Updated: 2019/12/04 11:53:38 by dromanic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
-#include <winbase.h>
 #include "libft.h"
 
-/*
-** on macOS detecting number of processors on machine:
-** <sys/param.h>, <sys/sysctl.h> needed for int func get_processors_num();
-*/
+#ifdef __unix__
 
-/*
-** on linux detecting number of processors on machine:
-** #include <sys/sysinfo.h>
-** and using:
-**	{
-**		return (sysconf(_SC_NPROCESSORS_ONLN));
-**	}
-*/
+# include <sys/param.h>
+# include <sys/sysctl.h>
 
-/*
-** on win32 detecting number of processors on machine:
-** #include <winbase.h>
-**	{
-**		SYSTEM_INFO		sysinfo;
-**
-**		GetSystemInfo(&sysinfo);
-**		return (sysinfo.dwNumberOfProcessors);
-**	}
-*/
+#elif defined(linux)
+
+# include <sys/sysinfo.h>
+
+#elif defined(_WIN32) || defined(WIN32)
+
+# include <winbase.h>
+
+#endif
+
+#ifdef __unix__
+
+int		ft_get_processors_num(void)
+{
+	int		nm[2];
+	int		count;
+	size_t	len;
+
+	len = 4;
+	nm[0] = CTL_HW;
+	nm[1] = HW_AVAILCPU;
+	sysctl(nm, 2, &count, &len, NULL, 0);
+	if (count < 1)
+	{
+		nm[1] = HW_NCPU;
+		sysctl(nm, 2, &count, &len, NULL, 0);
+		if (count < 1)
+			count = 1;
+	}
+	return (count);
+}
+
+#elif defined(linux)
+
+int		ft_get_processors_num(void)
+{
+	return (sysconf(_SC_NPROCESSORS_ONLN));
+}
+
+#elif defined(_WIN32) || defined(WIN32)
 
 int		ft_get_processors_num(void)
 {
@@ -46,3 +66,5 @@ int		ft_get_processors_num(void)
 	GetSystemInfo(&sysinfo);
 	return (sysinfo.dwNumberOfProcessors);
 }
+
+#endif
